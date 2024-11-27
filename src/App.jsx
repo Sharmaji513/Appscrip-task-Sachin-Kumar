b7ac0e2875326d9888367cc90ac23f599985b190
sachin <sk800968@gmail.com>
2024-11-27 13:10:53 +0530

    added MainConntent

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/Hero/HeroSection";
+import MainContent from "./components/Maincontent/Maincontent";
 
 function App() {
   return (
     <>
       <Header />
       <HeroSection />
+      <MainContent/>
     </>
   );
 }
